ae8163ef96110e955a6f73c8436449358504f041
hacpjc <hacpjc@gmail.com>
2018-07-15 09:37:20 +0800

    Add hexdump

--- Makefile.mk
+++ Makefile.mk
@@ -5,4 +5,8 @@ fio-obj-y += fio_easyrw.o
 fio-obj-y += fio_lock.o
 obj-y += $(addprefix fio/, $(fio-obj-y))
 
+hexdump-obj-y :=
+hexdump-obj-y += hexdump.o
+obj-y += $(addprefix hexdump/, $(hexdump-obj-y))
+
 #;
